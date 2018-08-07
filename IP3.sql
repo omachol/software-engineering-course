eafe4070f53250d4f4356442e5f48fd54d8eedf8
omachol <40063743+omachol@users.noreply.github.com>
2018-08-07 03:16:39 -0700

    Add files via upload

---
+++ IP3.sql
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+    <title>phpMyAdmin</title>
+    <link rel="stylesheet" type="text/css" href="phpmyadmin.css.php?token=c2b3ab4b8414c0c598e2f95c22193da4&amp;js_frame=right&amp;nocache=4033916589" />
+    <link rel="stylesheet" type="text/css" href="print.css" media="print" />
+    <meta name="robots" content="noindex,nofollow" />
+</head><body><p>export.php: Missing parameter: what <a href="./Documentation.html#faqmissingparameters" target="documentation"> (FAQ 2.8)</a><br />export.php: Missing parameter: export_type <a href="./Documentation.html#faqmissingparameters" target="documentation"> (FAQ 2.8)</a><br /></p></body></html>
